0c30bee514d9f618232d61e5548325aeee1d05b6
salsabl22 <salsabilaa2285@gmail.com>
2026-08-21 12:00:00 +0700

    commit awal

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-600">
+      CrewFlow Stok Opname
+      </h1>
+    </>
+  )
+}
+
+export default App
